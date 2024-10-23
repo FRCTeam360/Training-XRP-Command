1964e24324da33d35e0ea622869d31e704f10dbd
James Taylor <james.taylor2@comcast.net>
2024-10-22 17:41:32 -0700

    Added initialize method

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class ExampleCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final XRPDrivetrain m_subsystem;
-  private double distanceInches = 12.0;
+  private double distanceInches = 20.0;
 
   /**
    * Creates a new ExampleCommand.
@@ -26,7 +26,9 @@ public class ExampleCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_subsystem.resetEncoders();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
